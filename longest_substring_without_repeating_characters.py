8d91471de70ec8b4c4e4efd95207dfc73057694b
dinghao-bd <619015618@qq.com>
2016-07-16 19:07:32 +0800

    ok

--- longest_substring_without_repeating_characters.py
+++ longest_substring_without_repeating_characters.py
@@ -1,41 +1,25 @@
+# coding=utf-8
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
         :rtype: int
         """
-        tmp = {}
-        max1 = 0
-        for index, value in enumerate(s):
-            if value in tmp.values():
-                for i in xrange(index, len(s)):
-                    index1 = None
-                    max2 = 0
-                    if index1 is None:
-                        index1 = self.getIndex(tmp, value)
-                    # print(index1)
-                    if s[i] == s[index1]:
-                        index1 += 1
-                        max2 += 1
-                        continue
-                    else:
-                        max1 = max2
-                        break
-            tmp[index] = value
-            # print tmp
+        tmp = ''
+        max_len = []
+        if len(s) == 0:
+            return 0
+        else:
+            for index, value in enumerate(s):
+                if value not in tmp:
+                    tmp += value
+                    max_len.append(len(tmp))
+                    continue
+                else:
+                    index1 = tmp.find(value)
+                    tmp = tmp[index1+1:]
+                    tmp += value
+                    max_len.append(len(tmp))
+                    continue
 
-        return max1
-
-    def getIndex(self, dict, value):
-        '''
-        :param dict:
-        :param value:
-        :return int:
-        '''
-        for i in xrange(len(dict)):
-            if dict[i] == value:
-                return i
-
-str1 = "aaaa"
-s = Solution()
-print s.lengthOfLongestSubstring(str1)
+            return max(max_len)
